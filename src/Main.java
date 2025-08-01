c0671162a5ac87be413086385722166604b4020f
Shotecaps <npgalagnara46671@liceo.edu.ph>
2025-08-01 10:01:56 +0800

    activity 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+
+        int totalNumber = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int number = scanner.nextInt();
+            totalNumber += number;
+        }
+
+        System.out.println("\nTotal Number: " + totalNumber);
+
+        scanner.close();
     }
 }
