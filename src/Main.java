a12949ffb6149403f3d5e550fc6eaef6ca486166
Nicholai Patrick C. Galagnara <npgalagnara46671@liceo.edu.ph>
2025-08-07 11:52:45 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+    }public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book();
+        book1.setBookDetails("dasdasda", "hgalanagara", 11);
+        System.out.println("Book No. 1");
+        book1.displayInfo();
+        book1.borrowBook();
+        book1.returnBook();
+
     }
+}
 }
