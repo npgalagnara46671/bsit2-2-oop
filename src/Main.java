2cd477ebf0a6cf6903e9ff44efb0d9b00522e111
Nicholai Patrick C. Galagnara <npgalagnara46671@liceo.edu.ph>
2025-08-08 09:46:22 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Student s1 = new Student("Stephen Curry", 20, "OOP", 85, 78, 92);
+        Student s2 = new Student("Michael Jordan", 19, "Platform Technology", 80, 72, 90);
+        Student s3 = new Student("Kobe Bryant", 18, "Ethics", 54, 58, 60);
+
+        Student[] students = {s1, s2, s3};
+        int passingCount = 0;
+
+        for (Student s : students) {
+            s.displayInfo();
+            double avg = s.calculateAverage();
+            System.out.printf("Average: %.2f\n", avg);
+            String letter = s.getLetterGrade();
+            System.out.println("Letter Grade: " + letter);
+            boolean passing = s.isPassing();
+            System.out.println("Status: " + (passing ? "PASSING" : "FAILING"));
+            System.out.println();
+            if (passing) passingCount++;
+        }
+
+        System.out.println("Summary: Total number of students who are passing: " + passingCount);
     }
 }
