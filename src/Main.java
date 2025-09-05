755f988c1a67636eaa75bda60c671c266a30c380
Nicholai Patrick C. Galagnara <npgalagnara46671@liceo.edu.ph>
2025-09-05 10:10:28 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        LibraryManager manager = new LibraryManager();
+        manager.showBooks();
+        manager.addBook();
+        manager.removeBook();
     }
 }
